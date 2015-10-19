82485d9cfb34b380a7b58789bb85b8b8ab41b44d
unknown <TQuass_be@dev1.devcodecamp.inc>
2015-10-19 17:24:42 -0500

    added files to repo

---
+++ Checkers/Checkers/GameBoard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public struct CellPosition
+    {
+        private int x;
+        private int y;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+    }
+    class GameBoard
+    {
+    }
+}
