bdd94ef024eb66cc4c5a6002de6bb2e2e02a55ac
unknown <TQuass_be@dev1.devcodecamp.inc>
2015-10-26 09:39:59 -0500

    changed client/server functionality

--- CheckersGameServer/CheckersGameServer/Program.cs
+++ CheckersGameServer/CheckersGameServer/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace CheckersGameServer
 {
@@ -12,11 +13,32 @@ namespace CheckersGameServer
     {
         static void Main(string[] args)
         {
+            TcpListener listener = new TcpListener(IPAddress.Any, 7777);
+            TcpClient client;
+            listener.Start();
+            Console.WriteLine("***Game Server Started***");
             while (true)
             {
-                TCPListener tcpListener = new TCPListener();
-                tcpListener.Listen();
+                client = listener.AcceptTcpClient();
+                ThreadPool.QueueUserWorkItem(ThreadProc, client);
             }
+        }
+        private static void ThreadProc (object obj)
+        {
+            var client = (TcpClient)obj;
+       
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received: " + dataReceived);
+
+            Console.WriteLine("Sending To Client: " + dataReceived);
+            nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            Console.ReadLine();
         }
     }
 }
